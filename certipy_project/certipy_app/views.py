38e861ad18ffd4ea9ed3bd407262a706ff50b390
Marcus Vinicius Miguel de Oliveira <marcusvinoli@gmail.com>
2023-11-22 19:05:06 -0300

    Fixes date insertion on DB.

--- certipy_project/certipy_app/views.py
+++ certipy_project/certipy_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Course
+from datetime import datetime
 
 # Create your views here.
 def home(request):
@@ -11,9 +12,9 @@ def courses(request):
 		new_course.title = request.POST.get('title')
 		new_course.workload = request.POST.get('workload')
 		new_course.teacher = request.POST.get('teacher')
-		new_course.date = request.POST.get('date')
+		new_course.date = datetime.strptime(request.POST.get('date'), '%Y-%m-%d').date()
 		new_course.save()
-	
+
 	courses = { 'courses': Course.objects.all() }
 	
 	return render(request, 'app/courses.html', courses)
