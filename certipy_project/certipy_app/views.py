9810ab70421eeb60fc61dee6e81ab73aad8fce7b
Marcus Vinicius Miguel de Oliveira <marcusvinoli@gmail.com>
2023-11-22 13:18:41 -0300

    Added POST handler for course URL.

--- certipy_project/certipy_app/views.py
+++ certipy_project/certipy_app/views.py
@@ -3,16 +3,26 @@ from .models import Course
 
 # Create your views here.
 def home(request):
-    return render(request, 'app/home.html')
+	return render(request, 'app/home.html')
 
 def courses(request): 
-    return render(request, 'app/courses.html')
+	if request.method == 'POST':
+		new_course = Course() 
+		new_course.title = request.POST.get('title')
+		new_course.workload = request.POST.get('workload')
+		new_course.teacher = request.POST.get('teacher')
+		new_course.date = request.POST.get('date')
+		new_course.save()
+	
+	courses = { 'courses': Course.objects.all() }
+	
+	return render(request, 'app/courses.html', courses)
 
 def templates(request): 
-    return render(request, 'app/templates.html')
+	return render(request, 'app/templates.html')
 
 def settings(request): 
-    return render(request, 'app/settings.html')
+	return render(request, 'app/settings.html')
 
 def certificates(request): 
-    return render(request, 'app/certificates.html')
+	return render(request, 'app/certificates.html')
